1bd24f6160160cb6c7762940f0b97c047af9cd0c
Alex Orear <alex.orear@gmail.com>
2017-06-27 19:33:23 -0500

    added view paths for /polls/(detail/result/vote)

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 # Create your views here.
+def detail(request, question_id):
+    return HttpResponse("You're looking at questions {0}." .format(question_id))
+
+def results(request, question_id):
+    response = "You're looking at the results of question {0}." .format(question_id)
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question {0}" .format(question_id))
