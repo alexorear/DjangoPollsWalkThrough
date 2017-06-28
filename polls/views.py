a4a72b29c2b6a7e36d177354e7690814e9a42bbf
Alex Orear <alex.orear@gmail.com>
2017-06-28 10:09:13 -0500

    Created results view/template. Refacted Index, Detail, Results views to generic views

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,38 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from .models import Question, Choice
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions"""
+        return Question.objects.order_by('-pub_date')[:5]
+
 # Create your views here.
-def detail(request, question_id):
-    return HttpResponse("You're looking at questions {0}." .format(question_id))
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'poll/detail.html'
 
-def results(request, question_id):
-    response = "You're looking at the results of question {0}." .format(question_id)
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'poll/results.html'
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question {0}" .format(question_id))
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        #redisplay for with error_message
+        return render(request, 'polls/detail.html', {'question' : question,
+        'error_message' : "You didn't select a choice"})
+
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
